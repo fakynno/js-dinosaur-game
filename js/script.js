f015da67a664278d5498bd6216190158fa5ca5d0
fakynno <fakynno@gmail.com>
2022-05-15 11:11:49 -0300

    Add logica para game-over

--- js/script.js
+++ js/script.js
@@ -52,6 +52,10 @@ function createCactus() {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
+            // verifica se o cactus bateu no dinossauro
+        } else if (cactusPosition > 0 && cactusPosition < 60) {
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
         } else {            
             // movimenta o cactus
             cactusPosition -= 10;
